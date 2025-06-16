c79bade7d24b8dd6f4b340272cdfb36ff94ade48
Girobusan <private>
2025-06-16 19:47:07 +0300

    +gulptask

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,16 @@ const rename = require("gulp-rename");
 const postcss = require("gulp-postcss");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
-var exec = require('child_process').exec;
-/*
+var exec = require("child_process").exec;
 
-
-gulp.task('task', function (cb) {
-  exec('ping localhost', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
+async function buildSite(cb) {
+  exec("node mukha.js", { cwd: "src" }, (err, out, stderr) => {
+    // console.log(out);
+    stderr && console.log(stderr);
+    err && console.error(err);
+    cb();
   });
-})
-*/
+}
 async function buildCSS() {
   return src("src/scss/style.scss")
     .pipe(sourcemaps.init())
@@ -24,22 +22,10 @@ async function buildCSS() {
     .pipe(dest("css/"));
 }
 
-async function buildDist() {
-  return src("dev/scss/amzng.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(postcss([require("autoprefixer")]))
-    .pipe(dest("css/"))
-    .pipe(dest("dist/"))
-    .pipe(postcss([require("postcss-minify")]))
-    .pipe(rename("amzng.min.css"))
-    .pipe(dest("css/"))
-    .pipe(dest("dist/"));
+async function watchCSS() {
+  return watch(["src/scss/*.scss", "src/site/config"], buildCSS);
 }
 
-async function watchFiles() {
-  return watch("dev/scss/*.scss", buildCSS);
-}
-
-exports.watch = watchFiles;
+exports.watchCSS = watchCSS;
 exports.css = buildCSS;
-exports.dist = buildDist;
+exports.buildSite = buildSite;
